2a9e7b87d65ca1d6aa19e779d1024ceffbbe1343
kiarash <kiarashs37@gmail.com>
2026-07-02 16:08:12 +0330

    add conform and flash nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+        -- Define your formatters by filetype
+        formatters_by_ft = {
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            typescript = { "prettierd", "prettier", stop_after_first = true },
+            javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+            typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+            css = { "prettierd", "prettier", stop_after_first = true },
+            html = { "prettierd", "prettier", stop_after_first = true },
+            json = { "prettierd", "prettier", stop_after_first = true },
+            markdown = { "prettierd", "prettier", stop_after_first = true },
+        },
+        -- Set up format-on-save
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
+    },
+}
